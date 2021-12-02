4448621efe058cfeace20d7f85e808a274c47937
Atakan Cengiz KURT <atakan1903@hotmail.com>
2021-12-02 19:55:21 +0300

    Initial Commit

---
+++ SayiTahminEtEgitimUygulamasi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SayiTahminEtEgitimUygulamasi
+//
+//  Created by Atakan Cengiz KURT on 2.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SayiTahminEtEgitimUygulamasi/SayiTahminEtEgitimUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayiTahminEtEgitimUygulamasiApp.swift
+//  SayiTahminEtEgitimUygulamasi
+//
+//  Created by Atakan Cengiz KURT on 2.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SayiTahminEtEgitimUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
